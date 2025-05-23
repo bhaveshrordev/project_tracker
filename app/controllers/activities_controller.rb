7069662eb94514e1939fd355d20e5f4086d4767b
bhaveshrordev <salujabhavesh24@gmail.com>
2025-05-23 19:58:09 +0530

    added project name validation and updated show UI

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,21 @@
+# app/controllers/activities_controller.rb
 class ActivitiesController < ApplicationController
   def create
+    @project = Project.find(params[:project_id])
+    @activity = @project.activities.build(
+      kind: params[:kind],
+      content: params[:content],
+      user: current_user
+    )
+
+    if @activity.kind == "status_change"
+      @project.update(status: @activity.content)
+    end
+
+    if @activity.save
+      redirect_to @project, notice: "Activity added!"
+    else
+      redirect_to @project, alert: "Something went wrong."
+    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   enum status: { pending: 0, in_progress: 1, completed: 2, archived: 3 }
 
   has_many :activities, dependent: :destroy
+
+  validates :name, presence: true
 end
