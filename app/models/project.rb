108aab034648aca4b3f304b451f658adc304daf8
bhaveshrordev <salujabhavesh24@gmail.com>
2025-05-23 20:32:33 +0530

    added callback to record activity

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,33 @@
 class Project < ApplicationRecord
   enum status: { pending: 0, in_progress: 1, completed: 2, archived: 3 }
 
+  belongs_to :user
   has_many :activities, dependent: :destroy
 
   validates :name, presence: true
+
+  after_create :log_creation_activity
+  after_update :log_status_change_activity
+
+  private
+
+  def log_creation_activity
+    activities.create!(
+      kind: "status_change",
+      content: status,
+      user: user
+    )
+  end
+
+  def log_status_change_activity
+    if saved_change_to_status?
+      activities.create!(
+        kind: "status_change",
+        content: status,
+        user: user
+      )
+    end
+  end
 end
+
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_23_134909) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_23_145450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_23_134909) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_23_134909) do
 
   add_foreign_key "activities", "projects"
   add_foreign_key "activities", "users"
+  add_foreign_key "projects", "users"
 end
